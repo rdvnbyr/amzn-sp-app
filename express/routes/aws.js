b72462c0424df4d527ffd1aee4690b9d9d7a0340
Ridvan Bayir <bayir@argutus.de>
2024-03-20 17:00:35 +0100

    Aktualisiere aws.js Route-Datei für das Erstellen von Einträgen

--- express/routes/aws.js
+++ express/routes/aws.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const router = express.Router();
 const spClient = require('../services/sp');
 
-router.get('/ping', (req, res) => {
-  res.json({ message: 'aws' });
-});
+const router = express.Router();
 
 router.post('/createListing', async (req, res) => {
   try {
     const sp = new spClient();
+
     const [error, listingItem] = await sp.createListing();
     if (error) throw error;
+
     res.status(200).json(listingItem);
   } catch (error) {
     res.status(500).json({ message: error.message });
